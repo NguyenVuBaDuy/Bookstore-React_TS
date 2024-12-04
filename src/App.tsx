96c14752904bba53f18744b595862240a87ff406
Ba Duy <nguyenvubaduy9a11a@gmail.com>
2024-12-04 13:09:15 +0700

    feat(app): integrate redux for counter functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-
+import { useDispatch } from "react-redux"
+import { useAppSelector } from './redux/hooks'
+import { decrement, increment } from "./redux/counter/counterSlice"
 const App = () => {
+
+  const dispatch = useDispatch()
+
+  const value: number = useAppSelector(state => state.counter.value)
+
   return (
-    <div>Welcome! I'm Ba Duy</div>
+    <>
+      <div>
+        <span style={{ padding: "20px", backgroundColor: "red", cursor: "pointer" }}
+          onClick={() => { dispatch(decrement()) }}
+        >minus</span>
+        <span style={{ padding: "20px", backgroundColor: "blue" }}>{value}</span>
+        <span style={{ padding: "20px", backgroundColor: "green", cursor: "pointer" }}
+          onClick={() => { dispatch(increment()) }}>plus</span>
+      </div>
+    </>
   )
 }
 
